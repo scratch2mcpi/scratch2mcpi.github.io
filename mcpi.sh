a1585aaa47a228e4af0c4315f48a7098f79d69f0
Junya Ishihara <junya@champierre.com>
2014-01-19 21:37:42 +0900

    if shortcut icon exists skip copying

--- mcpi.sh
+++ mcpi.sh
@@ -9,8 +9,13 @@ tar zxvf /tmp/minecraft-pi-0.1.1.tar.gz
 rm /tmp/minecraft-pi-0.1.1.tar.gz
 
 # Copy Minecraft Pi shortcut on Desktop
-echo "\n\033[36m\033[1mCopying Minecraft Pi shortcut on Desktop...\033[00m\n"
-wget -P "/home/pi/Desktop" http://scratch2mcpi.github.io/mcpi.desktop
+if [ -f /home/pi/Desktop/mcpi.desktop ]
+then
+	echo "\n\033[33m\033[1mMinecraft Pi shortcut on Desktop exists. Skipped copying.\033[00m\n"
+else
+	echo "\n\033[36m\033[1mCopying Minecraft Pi shortcut on Desktop...\033[00m\n"
+	wget -P "/home/pi/Desktop" http://scratch2mcpi.github.io/mcpi.desktop
+fi
 
 # Download Python samples
 echo "\n\033[36m\033[1mDownloading Python samples...\033[00m\n"
